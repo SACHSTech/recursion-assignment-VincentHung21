d7d4923fbbca547e149adebe45a382c84ca4e535
Vincent Hung <vincent.hung21@ycdsbk12.ca>
2020-12-04 16:15:20 +0000

    Test 1 Green

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -2,7 +2,7 @@ package recursionAssignment;
 
 public class Recursion{
   public static int count7(int n) {
-    if (n / 10 == 0) {
+    if (n == 0) {
       return 0;
     } else if (n % 10 == 7) {
       return 1 + Recursion.count7(n / 10);
